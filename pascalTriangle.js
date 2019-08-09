73fb535b65311d689c3bb1d0446cff926666befd
ramyaNg <38860717+ramyaNg@users.noreply.github.com>
2019-08-09 13:21:49 +0530

    creating file

---
+++ pascalTriangle.js
@@ -0,0 +1,19 @@
+function printPascalTriangle(rows) {
+  var arr = [];
+
+  for (var i = 0; i < rows; i++) {
+    arr[i] = [];
+
+    for (var j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        arr[i][j] = 1;
+      } else {
+        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(printPascalTriangle(5));
